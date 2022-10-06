dc597ce46670bc1daf0c89420c11e25dbf063976
NikolayAnanyev <ananievnikolai@mail.ru>
2022-10-06 15:21:16 +0400

    Exercise 2.1.2 completed.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloBean1.getMessage());
+
+        HelloWorld helloBean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(catBean1.getMessage());
+
+        System.out.println(helloBean1 == helloBean2);
+        System.out.println(catBean1 == catBean2);
     }
 }
